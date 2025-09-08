8408dd3f57938c27b79b4e9a26621fec60a57edc
Harivaradhan <m.harivaradhan.12345@gmail.com>
2025-09-09 02:01:39 +0530

    done

--- src/main/java/com/management/filter/CorsFilter.java
+++ src/main/java/com/management/filter/CorsFilter.java
@@ -8,24 +8,29 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebFilter("/*")
 public class CorsFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {}
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+ @Override
+public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
 
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    
+    System.out.println(">>> CorsFilter applied for: " + ((HttpServletRequest) request).getRequestURI());
+    HttpServletResponse res = (HttpServletResponse) response;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-        chain.doFilter(request, response);
-    }
+    chain.doFilter(request, response);
+}
 
     @Override
     public void destroy() {}
