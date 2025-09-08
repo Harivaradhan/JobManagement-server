74c7850adcfbbdf1d534647567a7c714404c6c97
jack10 <76940581+Harivaradhan@users.noreply.github.com>
2025-09-09 01:00:26 +0530

    Update CorsFilter.java

--- src/main/java/com/management/filter/CorsFilter.java
+++ src/main/java/com/management/filter/CorsFilter.java
@@ -20,6 +20,7 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
+                System.out.println(">>> CorsFilter applied for: " + req.getRequestURI());
         HttpServletResponse res = (HttpServletResponse) response;
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
