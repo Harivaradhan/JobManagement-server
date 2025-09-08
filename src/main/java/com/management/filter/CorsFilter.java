d3b36b4225c44267b9ba4539982080768c49418e
jack10 <76940581+Harivaradhan@users.noreply.github.com>
2025-09-09 01:53:32 +0530

    Update CorsFilter.java

--- src/main/java/com/management/filter/CorsFilter.java
+++ src/main/java/com/management/filter/CorsFilter.java
@@ -16,18 +16,19 @@ public class CorsFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {}
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+ @Override
+public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
 
-                System.out.println(">>> CorsFilter applied for: " + req.getRequestURI());
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    
+    System.out.println(">>> CorsFilter applied for: " + ((HttpServletRequest) request).getRequestURI());
+    HttpServletResponse res = (HttpServletResponse) response;
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-        chain.doFilter(request, response);
-    }
+    chain.doFilter(request, response);
+}
 
     @Override
     public void destroy() {}
