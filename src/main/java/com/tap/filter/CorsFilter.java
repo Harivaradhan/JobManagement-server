fd91908d735370d75f57a12c59e2f8c893dde144
Harivaradhan <m.harivaradhan.12345@gmail.com>
2025-09-09 09:51:07 +0530

    added the .war file to target

---
+++ src/main/java/com/tap/filter/CorsFilter.java
@@ -0,0 +1,41 @@
+package com.tap.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // optional
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        res.setHeader("Access-Control-Allow-Origin", "https://job-management-rosy.vercel.app"); 
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        System.out.println(">>> CORS filter applied");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // optional
+    }
+}
